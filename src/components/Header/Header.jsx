53cd8d2f179e4b0e777e0127372c6a459d314265
ahsan200t <ahsanpekua200@gmail.com>
2024-03-12 21:53:52 +0600

    layout section added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
+import profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
-        <div>
-            <h1 className='text-4xl font-bold'>Knowledge Cafe</h1> 
+        <div className='flex justify-between items-center container mx-auto p-4 border-b-2'>
+            <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+            <img src={profile} alt="" /> 
         </div>
     );
 };
